c556ec502c4b3e9d9aa2d0e6e6c1f7c719265a55
Dmitriy Shaydurov <dmitriy.shaydurov@gmail.com>
2025-04-26 15:53:10 +0700

    First commit

---
+++ resources/js/Plugins/quasar.js
@@ -0,0 +1,10 @@
+import { Quasar } from 'quasar'
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+export default {
+    plugins: {
+        // Здесь можно подключить, например, Notify, Dialog, Loading
+        // Пример позже если надо
+    },
+}
